2ecd2c68cbe3721b35a86682efb4c0e0a89b5a4d
Phlipper <marzullop73@gmail.com>
2020-12-04 13:06:38 -0500

    ran schema file  and seeds.sql from mysql cli to create database

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,13 @@
 --      * **burger_name**: a string.
 --      * **devoured**: a boolean.
 
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
