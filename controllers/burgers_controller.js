14e5ab4f4877fde9bd401ee97376ebcc86a958ce
Phlipper <marzullop73@gmail.com>
2020-12-04 17:27:44 -0500

    added route and listen function in server.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -32,7 +32,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  cat.update({
+  burger.update({
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
--- server.js
+++ server.js
@@ -2,3 +2,13 @@
 const express = require("express");
 
 const DB Setup = require ("DB Setup");
+
+const routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
